f2c13b3f25aff73ba4807e065b859decb9fcbca6
hasankirmizielma-design <hasankirmizielma@googlemail.com>
2026-04-11 23:04:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 import os
-TOKEN = os.getenv("MTQ5MjM4NTk5MDIzMTE5NTY0OA.GuXZkr.8B-U6_ff4xtIIPpYEICKi0nxOgPZVE60-vqTT8")
+TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
